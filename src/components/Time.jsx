f2a4f784838be1bf259521626db894544cf4eac0
iroda0103 <iroda3242@gmail.com>
2023-10-30 22:05:45 +0500

    feat:hozirgi vaqtni chiqarib beruvchi komponentani qo'sh

---
+++ src/components/Time.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function addZero(time) {
+  return time.toString().length == 1 ? "0" + time : time;
+}
+
+export const Time = () => {
+  const [date, setDate] = useState(new Date());
+
+  const time = `${addZero(date.getDay())}.
+  ${addZero(date.getMonth() + 1)}.
+  ${date.getFullYear()}, 
+  ${addZero(date.getHours())}:
+  ${addZero(date.getMinutes())}`;
+
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+
+  return <div>Bugun:{time}</div>;
+};
+
+export default Time;
