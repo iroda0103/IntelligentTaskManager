e6d9f87221e67dc7505be283a5e658f1eff587e9
iroda0103 <iroda3242@gmail.com>
2023-10-31 19:18:24 +0500

    refactor:addZero funckiyasini global scopedan local scopega o'tkaz

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-function addZero(time) {
-  return time.toString().length == 1 ? "0" + time : time;
-}
-
 export const Time = () => {
   const [date, setDate] = useState(new Date());
+
+  function addZero(time) {
+    return time.toString().length == 1 ? "0" + time : time;
+  }
 
   const time = `${addZero(date.getDate())}.
   ${addZero(date.getMonth() + 1)}.
